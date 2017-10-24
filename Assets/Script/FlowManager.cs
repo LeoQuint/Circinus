36c8587a71479f1ec07c625b15a7cc0ea3402894
Leonard <leonardquintal@gmail.com>
2017-10-24 18:37:34 -0400

    Init

---
+++ Assets/Script/FlowManager.cs
@@ -0,0 +1,68 @@
+﻿//////////////////////////////////////////
+//	Create by Leonard Marineau-Quintal  //
+//		www.leoquintgames.com			//
+//////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum eScene
+{
+    NULL,
+    Ship,
+    Galaxy,
+    World,
+    Maine_Menu,
+    Count
+}
+
+public class FlowManager : Singleton {
+
+    ////////////////////////////////
+    ///			Constants		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Statics			 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///	  Serialized In Editor	 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Public			 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Protected		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Private			 ///
+    ////////////////////////////////
+
+    #region Unity API
+    #endregion
+
+    #region Public API
+
+    public void LoadScene(eScene scene, LoadSceneMode loadMode)
+    {
+        SceneManager.LoadScene(scene.ToString(), loadMode);
+    }
+
+    public void LoadSceneAsync(eScene scene, LoadSceneMode loadMode)
+    {
+        SceneManager.LoadSceneAsync(scene.ToString(), loadMode);
+    }
+
+    #endregion
+
+    #region Protect
+    #endregion
+
+    #region Private
+    #endregion
+}
---
+++ Assets/Script/Singleton.cs
@@ -0,0 +1,58 @@
+﻿//////////////////////////////////////////
+//	Create by Leonard Marineau-Quintal  //
+//		www.leoquintgames.com			//
+//////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+    ////////////////////////////////
+    ///			Constants		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Statics			 ///
+    ////////////////////////////////
+    public static Singleton Instance;
+    ////////////////////////////////
+    ///	  Serialized In Editor	 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Public			 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Protected		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Private			 ///
+    ////////////////////////////////
+
+    #region Unity API
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    #endregion
+
+    #region Public API
+    #endregion
+
+    #region Protect
+    #endregion
+
+    #region Private
+    #endregion
+}
