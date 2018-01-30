96ba4c2f5762a7e2e26d5ecd90c377b7e9028e9d
Leonard <leonardquintal@gmail.com>
2018-01-29 19:11:59 -0500

    Camera Galaxy

--- Assets/Script/Galaxy/Galaxy_Camera.cs
+++ Assets/Script/Galaxy/Galaxy_Camera.cs
@@ -19,7 +19,14 @@ public class Galaxy_Camera : MonoBehaviour {
     ////////////////////////////////
     ///	  Serialized In Editor	 ///
     ////////////////////////////////
-
+    [SerializeField]
+    private float m_CameraLerpSpeed = 5f;
+    [SerializeField]
+    private float m_CameraZoomSpeed = 10f;
+    [SerializeField]
+    private float m_CameraZoomSmoothingRatio = 0.5f;
+    [SerializeField]
+    private AnimationCurve m_CameraLerpSpeedToZoom;
     ////////////////////////////////
     ///			Public			 ///
     ////////////////////////////////
@@ -34,10 +41,16 @@ public class Galaxy_Camera : MonoBehaviour {
     private float m_MinZPosition = -5f;
     private float m_MaxZPosition = -40f;
 
+    private Vector3 m_InputMovement = Vector3.zero;
+    private float m_ZMovement = 0f;
     #region Unity API
     private void Update()
     {
-
+        GetInput();
+        Vector3 position = Vector3.Lerp(transform.position, transform.position + m_InputMovement, Time.deltaTime * m_CameraLerpSpeed);
+        position.z = Mathf.Lerp(transform.position.z, transform.position.z + m_ZMovement, m_CameraZoomSmoothingRatio);
+        position.z = Mathf.Clamp(position.z, m_MaxZPosition ,m_MinZPosition);
+        transform.position = position;
     }
     #endregion
 
@@ -47,7 +60,16 @@ public class Galaxy_Camera : MonoBehaviour {
     #region Protect
     protected void GetInput()
     {
+        //Temp inputs
+        if (Input.GetButton("Fire2"))
+        {
+            float x = transform.position.z / m_MaxZPosition;
+            float lerpSpeed = m_CameraLerpSpeedToZoom.Evaluate(x);
+            m_InputMovement.x = -Input.GetAxis("Horizontal") * lerpSpeed;
+            m_InputMovement.y = -Input.GetAxis("Vertical") * lerpSpeed;
+        }
 
+        m_ZMovement = Input.GetAxis("Mouse ScrollWheel") * m_CameraZoomSpeed;
     }
     #endregion
 
