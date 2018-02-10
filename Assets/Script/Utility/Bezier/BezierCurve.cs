ec432a287fadf5aeb6658f869456acddd31e73a9
Leonard <leonardquintal@gmail.com>
2018-02-10 15:12:27 -0500

    more bezier fun!

--- Assets/Script/Utility/Bezier/BezierCurve.cs
+++ Assets/Script/Utility/Bezier/BezierCurve.cs
@@ -13,7 +13,7 @@ public class BezierCurve : MonoBehaviour {
     ////////////////////////////////
     ///			Constants		 ///
     ////////////////////////////////
-
+    private const int POINTS_PER_BEZIER = 50;
     ////////////////////////////////
     ///			Statics			 ///
     ////////////////////////////////
@@ -29,6 +29,14 @@ public class BezierCurve : MonoBehaviour {
     protected float _Width = 5f;
     [SerializeField]
     private float _HandleSize = 0.5f;
+    [SerializeField]
+    private bool _ForceSmoothPath = true;
+    [SerializeField]
+    [InEditorReadOnly]
+    private List<Vector3> m_Path = new List<Vector3>();
+    [SerializeField]
+    [InEditorReadOnly]
+    private float m_Length = -1f;
     ////////////////////////////////
     ///			Public			 ///
     ////////////////////////////////
@@ -67,16 +75,19 @@ public class BezierCurve : MonoBehaviour {
     {
         get { return m_NumberOfBezierInPath;  }
     }
-    #region Unity API
-    protected void Update()
-    {
-        
-    }    
 
-    protected void OnDrawGizmosSelected()
+    public float Length
     {
-        
+        get
+        {
+            if (m_Length == -1f)
+            {
+                console.logWarning("Bezier length not calculated. Use PreCalculatePoints to calculate.");
+            }
+            return m_Length;
+        }
     }
+    #region Unity API
     #endregion
 
     #region Public API
@@ -89,6 +100,7 @@ public class BezierCurve : MonoBehaviour {
         _Points.Add(new Vector3(_Points[_Points.Count - 1].x + 1f, _Points[_Points.Count - 1].y + 1f, _Points[_Points.Count - 1].z));
         _Points.Add(new Vector3(_Points[_Points.Count - 1].x + 1f, _Points[_Points.Count - 1].y + 1f, _Points[_Points.Count - 1].z));
         ++m_NumberOfBezierInPath;
+        SceneView.RepaintAll();
     }
     /// <summary>
     /// Remove a curve
@@ -101,29 +113,57 @@ public class BezierCurve : MonoBehaviour {
             _Points.RemoveAt(_Points.Count - 1);
             _Points.RemoveAt(_Points.Count - 1);
             --m_NumberOfBezierInPath;
+            SceneView.RepaintAll();
         }
+    }
+
+    [ContextMenu("PreCalculatePoints")]
+    public void PreCalculatePoints()
+    {
+        m_Path.Clear();
+        m_Path.Capacity = m_NumberOfBezierInPath * POINTS_PER_BEZIER;
+        m_Length = 0f;
+        for (int i = 0; i < m_NumberOfBezierInPath; ++i)
+        {
+            for (int j = 0; j <= POINTS_PER_BEZIER; ++j)
+            {
+                m_Path.Add(GetPointOnCurve(((float)j / (float)POINTS_PER_BEZIER), i));
+            }
+            //Calculate the length of each bezier.
+            m_Length += CalculateBezierLength(i);
+        }        
     }
     #endregion
 
     #region Protect
-    [ContextMenu("PreCalculatePoints")]
-    protected void PreCalculatePoints()
-    {
-        //todo precalculate x points for performance.
-    }
+
     #endregion
 
     #region Private
-    Vector3 GetPointOnCurve(float t)
+    private Vector3 GetPointOnCurve(float t, int bezier = 0)
     {
         float u = 1f - t;
         float t2 = t * t;
         float u2 = u * u;
         float u3 = u2 * u;
         float t3 = t2 * t;
-
-        Vector3 point = (u3) * _Points[0] + (3f * u2 * t) * _Points[1] + (3f * u * t2) * _Points[2] + (t3) * _Points[3];
+        Debug.Log(t + " " +  bezier);
+        Vector3 point = (u3) * _Points[0 + (bezier * 3)] +
+                        (3f * u2 * t) * _Points[1 + (bezier * 3)] +
+                        (3f * u * t2) * _Points[2 + (bezier * 3)] +
+                        (t3) * _Points[3 + (bezier * 3)];
         return point;
+    }
+
+    private float CalculateBezierLength(int bezier = 0)
+    {
+        float length = 0f;
+        for (int i = 0; i < POINTS_PER_BEZIER; ++i)
+        {
+            length += Vector3.Distance(m_Path[i + (bezier * POINTS_PER_BEZIER)], m_Path[i + 1 + (bezier * POINTS_PER_BEZIER)]);
+        }
+        Debug.Log(bezier + " " + length);
+        return length;
     }
     #endregion
 }
@@ -131,7 +171,9 @@ public class BezierCurve : MonoBehaviour {
 [CustomEditor(typeof(BezierCurve))]
 public class EditorBezierDrawer : Editor
 {
-    
+    private Vector3 controlPointDelta = Vector3.zero;
+    private Vector3 tangentDirection = Vector3.zero;
+
     protected void OnSceneGUI()
     {
         BezierCurve curve = target as BezierCurve;
@@ -155,7 +197,39 @@ public class EditorBezierDrawer : Editor
 
         for (int i = 0; i < curve.Points.Count; ++i)
         {
-            newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+            if (i % 3 == 0)//Control points
+            {
+                newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+                controlPointDelta = newPositions[i] - curve.Points[i];
+                if (i > 0)
+                {
+                    newPositions[i - 1] += controlPointDelta;
+                }
+                if (newPositions.Count > i + 2)
+                {
+                    newPositions[i + 1] += controlPointDelta;
+                    curve.Points[i + 1] = newPositions[i + 1];
+                }
+            }
+            else//tangents
+            {
+                /*if (i % 3 == 1 && i > 1)//after the control point
+                {
+                    newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+                    tangentDirection = (newPositions[i] - newPositions[i - 1]).normalized;
+                    
+                    newPositions[i - 2] -= tangentDirection * (newPositions[i] - curve.Points[i]).magnitude;
+                }
+                else if (i % 3 == 2 && i + 2 < newPositions.Count)//before the control point
+                {
+                    newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+                }
+                else
+                {
+                    newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+                }*/
+                newPositions[i] = Handles.FreeMoveHandle(curve.Points[i], Quaternion.identity, curve.HandleSize, Vector3.zero, Handles.RectangleHandleCap);
+            }            
         }
         if (EditorGUI.EndChangeCheck())
         {
